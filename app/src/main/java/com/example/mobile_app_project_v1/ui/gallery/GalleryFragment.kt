283a1d8cb18a9b5fe379b1dec8cc3bf351677c5f
Amir <amirdavis23x@gmail.com>
2023-04-24 13:32:37 -0400

    Added library to handle loading pdfs in gradle.app. Added android.enableJetifier=true to gradle properties bc jcenter is depreciated. Changing GalleryFramgent.kt to see if I can get a pdf to show up.

--- app/src/main/java/com/example/mobile_app_project_v1/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/mobile_app_project_v1/ui/gallery/GalleryFragment.kt
@@ -4,10 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.mobile_app_project_v1.R
 import com.example.mobile_app_project_v1.databinding.FragmentGalleryBinding
+import com.github.barteksc.pdfviewer.PDFView
 
 class GalleryFragment : Fragment() {
 
@@ -17,22 +18,21 @@ class GalleryFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    /*override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }*/
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+
+        //val galleryViewModel =
+         //   ViewModelProvider(this).get(GalleryViewModel::class.java)
 
         _binding = FragmentGalleryBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
